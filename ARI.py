e9a8edc76e4b44d1b0ca903eb540fa39385c52d3
George Doykan <George Doykan>
2017-05-30 19:27:09 -0500

    updated main file ARI.py

--- ARI.py
+++ ARI.py
@@ -1,6 +1,5 @@
 import sys
 
-
 def numWords(text):
     #returns number of words in text file
 
@@ -16,12 +15,13 @@ def numSentences(text):
 
 def numChars(text):
     #return number of character in text file
-
-    return len(text)
+    word_list = text.split()
+    char_list = [len(w) for w in word_list]
+    return sum(char_list)
 
 def avgLetters(text):
     #returns average number of letters per 100 words in a text file
-    #uses multiple lists to calculate this
+    #uses a list of 100 word chunks to calculate this
 
     word_list = text.split()
     intervals = range(0, len(word_list), 100)
@@ -29,26 +29,46 @@ def avgLetters(text):
     lettersList = []
     for n in range(0, len(intervals)):
         words = [len(i) for i in word_chunks[n]]
-        letters = sum(words)/10
+        letters = sum(words)
         lettersList.extend([letters])
     L = sum(lettersList)
-    return L/len(word_chunks)
+    return float(L/len(word_chunks))
+
+def avgSentences(text):
+    #takes entire text and returns average number of sentences per 100 words
+    word_list = text.split()
+    intervals = range(0, len(word_list), 100)
+    word_chunks = [word_list[n:n+100] for n in intervals]
+    sentencesList = []
+    for n in range(0,len(intervals)):
+        sentences = [word.count(".") for word in word_chunks[n]]
+        total = sum(sentences)
+        sentencesList.extend([total])
+    S = sum(sentencesList)
+    return float(S/len(word_chunks))
+
+def sortAlgo(algo, text):
+    #takes in an algorithm type and text and prints a readability index
+    if sys.argv[1] == "ARI":
+        num_char = float(numChars(text))
+        num_words = float(numWords(text))
+        num_sentences = float(numSentences(text))
+        ARI = (4.71 * (num_char/num_words)) + (0.5 * (num_words/num_sentences)) - 21.43
+        print ARI
+    elif sys.argv[1] == "Coleman-Liau":
+        L = avgLetters(text)
+        S = avgSentences(text)
+        index = float((0.0588*L)-(.296*S) - 15.8)
+        print index
+    else:
+        print ('Sorry algorithm not recognized. Type "ARI or "Coleman-Liau"')
 
 def main():
     filename = sys.argv[2]
     with open(filename, 'r') as input_file:
         text = input_file.read()
-    if sys.argv[1] == "ARI":
-        num_char = numChars(text)
-        num_words = numWords(text)
-        num_sentences = numSentences(text)
-        ARI = 4.71*(num_char/num_words)+ (.5*(num_words/num_sentences)-21.43)
-        print ARI
-    elif sys.argv[1] == "Coleman-Liau":
-        L = avgLetters(text)
-        #TODO make function to find avg number of sentences
-    else:
-        pass
+    sortAlgo(sys.argv[1], text)
+
 
 
 if __name__ == "__main__":
